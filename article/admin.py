4768d4a9daf891650f49038de09b707ed34fae84
faalin <602500355@qq.com>
2015-10-11 22:34:29 +0800

    'button'

--- article/admin.py
+++ article/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from article.models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+	"""docstring for ArticleAdmin"""
+	list_display = ('title','category','date_time')
+	list_filter = ('date_time',)
+	search_fields = ('category',)
+
 admin.site.register(Article)
